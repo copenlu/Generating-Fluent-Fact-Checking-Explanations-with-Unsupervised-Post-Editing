9a70b535ce20d8980d335f65efc7be62f99b90e2
Shailza Jolly <shailzajolly@gmail.com>
2021-01-25 11:45:02 +0100

    Editor code added

---
+++ editor.py
@@ -0,0 +1,50 @@
+from transformers import RobertaTokenizer, RobertaForMaskedLM
+import torch
+import numpy as np
+
+
+class RobertaEditor():
+    def __init__(self, model_dir="/root/dbox/mlm_roberta10/"):
+        self.model_dir = model_dir
+        self.tokenizer = RobertaTokenizer.from_pretrained(self.model_dir)
+        self.model = RobertaForMaskedLM.from_pretrained(self.model_dir, return_dict=True)
+        self.ops_map = [self.insert, self.replace, self.delete]
+        self.mask_vocab_idx = 50264
+        print("Editor built")
+
+    def cuda(self):
+        self.model.to('cuda')
+    
+    def edit(self, inputs, ops, positions):
+
+        masked_inputs = np.array([self.ops_map[op](inp, position) for inp, op, position, in zip(inputs, ops, positions)])
+        insert_and_replace_inputs = masked_inputs[np.where(ops<2)]
+        insert_and_replace_outputs = self.generate(insert_and_replace_inputs.tolist())
+        masked_inputs[np.where(ops < 2)] = insert_and_replace_outputs
+
+        return masked_inputs
+
+    def generate(self, input_texts):
+        inputs = {k:v.to('cuda') for k, v in self.tokenizer(input_texts, padding=True, return_tensors="pt").items()}
+        mask_idxs = (inputs["input_ids"]==self.mask_vocab_idx).long().max(dim=1).indices
+        outputs = self.model(**inputs)
+        mask_words = self.get_word_at_mask(outputs, mask_idxs)
+        return np.array([input_text.replace(" <mask>", mask_word) for input_text, mask_word in zip(input_texts, mask_words)])
+
+
+    def insert(self, input_texts, mask_idx):
+        input_texts_with_mask_list = input_texts.split()[:mask_idx] + ["<mask>"] + input_texts.split()[mask_idx:]
+        return " ".join(input_texts_with_mask_list)
+
+    def replace(self, input_texts, mask_idx):
+        input_texts_with_mask_list = input_texts.split()[:mask_idx] + ["<mask>"] + input_texts.split()[mask_idx + 1:]
+        return " ".join(input_texts_with_mask_list)
+
+    def delete(self, input_texts, mask_idx):
+        input_texts_with_mask_list = input_texts.split()[:mask_idx] + input_texts.split()[mask_idx + 1:]
+        return " ".join(input_texts_with_mask_list)
+
+    def get_word_at_mask(self, output_tensors, mask_idxs):
+
+        mask_idxs = mask_idxs.unsqueeze(dim=1)
+        return [self.tokenizer.decode(word_idx) for word_idx in torch.argmax(output_tensors.logits, dim=2).gather(1, mask_idxs).squeeze().cpu().numpy().tolist()]
