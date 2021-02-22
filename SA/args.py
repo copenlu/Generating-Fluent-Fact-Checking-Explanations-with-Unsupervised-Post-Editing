b5f6a5977d241868f08d7f3abe890d4fa8920f6f
Shailza Jolly <shailzajolly@gmail.com>
2021-02-22 08:43:43 +0100

    Added NLI objective, length penality, sentence similarity in scoring function

--- SA/args.py
+++ SA/args.py
@@ -3,11 +3,14 @@ import argparse
 def get_sa_args():
 
     args_dict = {
-        "t_init": 3e2,
+        "seed": 33,
+        "t_init": 3e-2,
         "C": 3e-4,
         "fluency_weight": 3,
-        "semantic_weight": 8,
-        "max_steps": 20,
+        "semantic_weight": 5,
+        "length_weight":8,
+        "nli_weight": 8,
+        "max_steps": 30,
     }
 
     args = argparse.Namespace(**args_dict)
